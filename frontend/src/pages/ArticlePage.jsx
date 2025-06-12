86a473fee2b393ccb106d5349c901aefa94ca2d0
Richard  Boappiah Asamoah <asab@0x.local>
2025-06-11 22:12:42 -0400

    all done

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,15 +1,29 @@
-import { useParams } from 'react-router-dom';
-import articles from '../article-content';
+import { useParams, useLoaderData } from "react-router-dom";
+import articles from "../article-content";
+import axios from "axios";
 
 export default function ArticlePage() {
   const { name } = useParams();
+  const { upvotes } = useLoaderData();
 
-  const article = articles.find(a => a.name === name);
+  const article = articles.find((a) => a.name === name);
+  if (!article) {
+    return <h1>Article not found!</h1>;
+  }
 
   return (
     <>
-    <h1>{article.title}</h1>
-    {article.content.map(p => <p key={p}>{p}</p>)}
+      <h1>{article.title}</h1>
+      <h2>Upvotes: {upvotes}</h2>
+      {article.content.map((p) => (
+        <p key={p}>{p}</p>
+      ))}
     </>
   );
+}
+
+export async function ArticleLoader({ params }) {
+  const response = await axios.get(`/api/articles/${params.name}`);
+  const { upvotes, comments } = response.data;
+  return { upvotes, comments };
 }
